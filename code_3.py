1583622cc649ecf3dbdedd6c9a797ab894b55269
Tejas <narayantejas1998@gmail.com>
2022-01-25 13:34:51 +0530

    loaded CVS into db

--- code_3.py
+++ code_3.py
@@ -1,10 +1,38 @@
 import urllib.request, json 
 import pandas as pd
-import csv
+import psycopg2
 
 with urllib.request.urlopen("https://jsonplaceholder.typicode.com/posts") as url:
     data = json.loads(url.read().decode())
     df = pd.DataFrame(data)
 
-df.to_csv('posts.csv', encoding='utf-8', index=False)
+#df.to_csv('posts.csv', encoding='utf-8', index=False)
 
+
+conn = psycopg2.connect(database="demodb",
+						user='postgres', password='123',
+						host='localhost', port='5432')
+
+conn.autocommit = True
+cursor = conn.cursor()
+
+sql = '''CREATE TABLE DETAILS(user_id int NOT NULL, post_id varchar(20),\
+post_title varchar(100), post_body varchar(500));'''
+
+
+cursor.execute(sql)
+
+sql2 = '''COPY details(user_id,post_id, post_title,post_body)
+FROM 'E:/Projects/Assignment_2/posts.csv'
+DELIMITER ','
+CSV HEADER;'''
+
+cursor.execute(sql2)
+
+sql3 = '''select * from details;'''
+cursor.execute(sql3)
+for i in cursor.fetchall():
+	print(i)
+
+conn.commit()
+conn.close()
